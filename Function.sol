063e25b1e2f46bf53c061e2e9c440f1400824865
Christiana Adeboye <161389197+Chriscess@users.noreply.github.com>
2024-03-27 23:55:33 +0100

    Update Function.sol

--- Function.sol
+++ Function.sol
@@ -1,1 +1,21 @@
-https://www.loom.com/share/f349681d9e574053ba368b886c2f4443?sid=3df8e4f2-8bb9-423b-9850-4c5e93f8c7d0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Functions {
+    function checkRequire(int input) public pure returns (int) {
+        require(input>5, "must be greater than 5"); 
+        return input;
+    } 
+    function checkAssert(int input) public pure returns (int) {
+        assert (input>4); 
+        return input;
+    } 
+
+    function checkRevert(int input) public pure returns (int) {
+            if(input<6) revert();
+            return input;
+    } 
+
+} 
+
+//0xd9145CCE52D386f254917e481eB44e9943F39138
